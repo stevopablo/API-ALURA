eb7cbe81bb200b4599bac45159fd39f83da55d9e
stevopablo <stevaopabllo@gmai.com>
2024-11-19 14:23:43 -0300

    post and findbyid

--- server.js
+++ server.js
@@ -1,5 +1,22 @@
 import express from "express";
 const app = express();
-app.get("/",(req,res)=>{res.send("Hellooo, World")})
-app.get("/api",(req,res)=>{res.status(200).send("endpoint api")})
+const posts = [
+    { id: 1, descricao: "Millie's christmas", imagem: "https://placecats.com/millie/300/150" },
+    { id: 2, descricao: "Gato tomando sol", imagem: "https://placecats.com/sunning/300/200" },
+    { id: 3, descricao: "Gato fazendo panqueca", imagem: "https://placecats.com/sunning/300/200" },
+]
+
+function findPostById(id){
+    return posts.findIndex((post)=>{
+        return post.id === Number(id)
+    })
+}
+app.use(express.json())
+app.get("/",(req,res)=>{res.send("Hello, World")})
+app.get("/post",(req,res)=>{res.status(200).json(posts)})
+
+app.get("/post/:id",(req,res)=>{
+    const index = findPostById(req.params.id)
+    res.status(200).json(posts[index])})
+
 app.listen(3000,()=>{console.log(`server listening...`)});
